7ab737e7dbf90addf2367514afcdee240f11263d
Sanggyu Kang <other.yuka@gmail.com>
2024-12-16 07:22:41 +0000

    refactor: enhance tooling setup and code formatting

--- src/client.ts
+++ src/client.ts
@@ -2,24 +2,24 @@ import type { AppType } from '@/server.ts'
 import { hc } from 'hono/client'
 
 const client = hc<AppType>('http://localhost:3000/', {
-    fetch: (input: RequestInfo | URL, requestInit?: RequestInit) => {
-        const headers = new Headers(requestInit?.headers)
-        headers.set('x-custom-header', 'x-custom-header-value')
-        return fetch(input, {
-            ...requestInit,
-            headers,
-        })
-    },
+	fetch: (input: RequestInfo | URL, requestInit?: RequestInit) => {
+		const headers = new Headers(requestInit?.headers)
+		headers.set('x-custom-header', 'x-custom-header-value')
+		return fetch(input, {
+			...requestInit,
+			headers,
+		})
+	},
 })
 
 const res = await client.api.users.$post({
-    json: {
-        name: 'yuka',
-        age: 99,
-    },
+	json: {
+		name: 'yuka',
+		age: 99,
+	},
 })
 
 if (res.ok) {
-    const data = await res.json()
-    console.log(data)
+	const data = await res.json()
+	console.log(data)
 }
