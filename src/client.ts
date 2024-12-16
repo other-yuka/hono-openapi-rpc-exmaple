6ac67b8d5c324b1a44182368635a8f89cdeaf72a
Sanggyu Kang <other.yuka@gmail.com>
2024-12-16 06:15:45 +0000

    feat: add zod validation and custom headers handling

--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,25 @@
 import type { AppType } from '@/server.ts'
 import { hc } from 'hono/client'
 
-const client = hc<AppType>('http://localhost:3000/')
+const client = hc<AppType>('http://localhost:3000/', {
+    fetch: (input: RequestInfo | URL, requestInit?: RequestInit) => {
+        const headers = new Headers(requestInit?.headers)
+        headers.set('x-custom-header', 'x-custom-header-value')
+        return fetch(input, {
+            ...requestInit,
+            headers,
+        })
+    },
+})
 
-const res = await client.users.$get()
+const res = await client.api.users.$post({
+    json: {
+        name: 'yuka',
+        age: 99,
+    },
+})
 
-console.log(res)
+if (res.ok) {
+    const data = await res.json()
+    console.log(data)
+}
--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,40 @@
 import { serve } from '@hono/node-server'
+import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
+import { z } from 'zod'
 
 const app = new Hono()
 
+const schema = z.object({
+	name: z.string().min(1),
+	age: z.number().min(1),
+})
+
 const routes = app
-    .get('/', (c) => {
-        console.table(c)
-        return c.text('Hello Hono!')
-    })
-    .get('/users', (c) => {
-        return c.json({
-            message: 'users',
-        })
-    })
-    .post('/api/users', (c) => {
-        return c.json({
-            message: 'young man is 20 years old',
-        })
-    })
+	.get('/', (c) => {
+		console.table(c)
+		return c.text('Hello Hono!')
+	})
+	.get('/api/users', (c) => {
+		return c.json({
+			user: 'users',
+		})
+	})
+	.post('/api/users', zValidator('json', schema), (c) => {
+		const { name, age } = c.req.valid('json')
+		const header = c.req.header()
+		console.log(header)
+		return c.json({
+			message: `Hello, ${name}. Your age is ${age}.`,
+		})
+	})
 
 const port = 3000
 console.log(`Server is running on http://localhost:${port}`)
 
 serve({
-    fetch: app.fetch,
-    port,
+	fetch: app.fetch,
+	port,
 })
 
 export type AppType = typeof routes
